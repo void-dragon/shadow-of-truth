f342ae3994280eefe0267b58ef2bc17f5c6d536d
zacharias <gojira83@gmx.de>
2021-03-14 16:49:18 +0100

    added percentage closer filtering to shadows

--- client/assets/shaders/phong.fragment.glsl
+++ client/assets/shaders/phong.fragment.glsl
@@ -28,10 +28,23 @@ float shadow_calculation() {
   vec3 projCoords = fs_in.FragPosLightSpace.xyz / fs_in.FragPosLightSpace.w;
   projCoords = projCoords * 0.5 + 0.5;
 
+  if (projCoords.z > 1.0) {
+    return 0.0;
+  }
+
   float closestDepth = texture(shadowMap, projCoords.xy).r;
   float currentDepth = projCoords.z;
   float bias = 0.05;
-  float shadow = (currentDepth - bias) > closestDepth  ? 1.0 : 0.0;
+  float shadow = 0.0;
+  vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+
+  for(int x = -1; x <= 1; ++x) {
+    for(int y = -1; y <= 1; ++y) {
+      float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+      shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
+    }    
+  }
+  shadow /= 9.0;
 
   return shadow;
 }
